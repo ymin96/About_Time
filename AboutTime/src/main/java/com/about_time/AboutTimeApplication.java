96a70c2577bcbe00a3a20d68ccf39acdf705f85a
Yonghwan Kim <ymin96@naver.com>
2019-10-23 01:27:31 +0900

    add carte.jsp

--- AboutTime/src/main/java/com/about_time/AboutTimeApplication.java
+++ AboutTime/src/main/java/com/about_time/AboutTimeApplication.java
@@ -2,12 +2,19 @@ package com.about_time;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AboutTimeApplication {
+public class AboutTimeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AboutTimeApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AboutTimeApplication.class);
 	}
 
 }
---
+++ AboutTime/src/main/webapp/WEB-INF/jsp/carte/carteInfo.jsp
@@ -0,0 +1,8 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jstl/core_rt" prefix="c"%>
+
+<body>
+	<div class="container">
+		<h1>메인화면이 표시될 곳입니다.</h1>
+	</div>
+</body>
