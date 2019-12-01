42b104a53149824429148885ddcd956468de8411
Yonghwan Kim <ymin96@naver.com>
2019-12-02 03:04:44 +0900

    add insertBoard function

--- AboutTime/src/main/java/com/about_time/community/controller/BoardController.java
+++ AboutTime/src/main/java/com/about_time/community/controller/BoardController.java
@@ -1,13 +1,19 @@
 package com.about_time.community.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.about_time.community.service.BoardService;
 import com.about_time.community.service.CommunityService;
+import com.about_time.community.vo.Board;
 
 @Controller
 public class BoardController {
@@ -15,15 +21,25 @@ public class BoardController {
 	@Autowired
 	CommunityService communityService;
 	
+	@Autowired
+	BoardService boardService;
+	
 	@RequestMapping(value = "/community/{title}/list", method = RequestMethod.GET)
 	public String boardList_get(@PathVariable("title")String title, Model model) {
 		model.addAttribute("title", title);
 		return "boardList";
 	}
 	
-	@RequestMapping(value = "/community/{title}/list/edit", method = RequestMethod.GET)
+	@RequestMapping(value = "/community/{title}/edit", method = RequestMethod.GET)
 	public String boardEdit_get(@PathVariable("title")String title, Model model) {
 		model.addAttribute("title",title);
 		return "boardEdit";
+	}
+	
+	@RequestMapping(value = "/community/{title}/edit", method = RequestMethod.POST)
+	public @ResponseBody void boardEdit_post(@RequestBody Board board, @PathVariable("title") String title, Principal principal) {
+		board.setUniversity(title);
+		board.setWriter(principal.getName());
+		boardService.insertBoard(board);
 	}
 }
--- AboutTime/src/main/java/com/about_time/community/mapper/BoardMapper.java
+++ AboutTime/src/main/java/com/about_time/community/mapper/BoardMapper.java
@@ -3,8 +3,10 @@ package com.about_time.community.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import com.about_time.community.vo.Board;
+
 @Mapper
 @Repository
 public interface BoardMapper {
-
+	public void insertBoard(Board board);
 }
